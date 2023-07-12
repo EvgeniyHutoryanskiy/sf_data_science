ec39ebf82d25b9a12986839fbb202a73640d393a
Evgeniy_X <evgeniy.hutoryanskiy@me.com>
2023-07-12 14:31:55 +0300

    commit

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -14,7 +14,7 @@ def random_predict(number:int=1) -> int:
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 200) # предполагаемое число
+        predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
     return(count)
@@ -43,6 +43,5 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
-if __name__ == '__main__':
-    score_game(random_predict)
+
+score_game(random_predict)
